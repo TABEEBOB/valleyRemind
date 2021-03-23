903dc2e1ecf87c0f3a04616b52919eef07d1b921
Arrey Tabe Ebob <60329084+TABEEBOB@users.noreply.github.com>
2021-03-22 21:31:24 -0400

    Add files via upload
    Login>>> username and password

---
+++ Welcome.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace valleyRemind
+{
+    public partial class Welcome : Form
+
+    {
+
+        private String user;
+        private String password;
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+
+        {
+            txtPassword.PasswordChar = '*';
+            user = txtLogin.Text;
+            password = txtPassword.Text;
+            if ((user == "v") && (password == "123"))
+            {
+                this.Hide();
+                Event_View eventView = new Event_View();
+                eventView.Show();
+
+                FormBorderStyle = FormBorderStyle.None;
+                WindowState = FormWindowState.Maximized;
+                TopMost = true;
+            }
+            else
+            {
+                MessageBox.Show("user ID or password incorrect");
+            }
+        }
+        private void Welcome_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                WindowState = FormWindowState.Normal;
+                TopMost = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+       
+    }
+}
